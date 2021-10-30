5e293d8a9e75e009541301b581b992b23b4464c2
Antonio Vitor Elias Swaid Jr <vitor.swaidjr@gmail.com>
2021-10-29 23:46:17 -0300

    acertos do .env e criação do env.sample

--- TrilhaDaEvolucao/forms.py
+++ TrilhaDaEvolucao/forms.py
@@ -1,8 +1,9 @@
 from django import forms
-from .models import VoluntarioParceiro
+from .models import VoluntarioParceiro, Familia, AreaPrograma
 
-class FormNovoAgendamento(forms.Form): 
+
+class FormNovoAgendamentoVoluntario(forms.Form): 
    nome = forms.CharField(required=True)
    email_origem = forms.EmailField(required=True, label = 'Entre com seu e-mail:')
    mensagem = forms.CharField(required=True)
-   vol_par = VoluntarioParceiro
+   vol_par = VoluntarioParceiro.objects
